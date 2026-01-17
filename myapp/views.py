ba4f609fe73316cf8e05b28528bf1d4a8b43f2e1
nwagboso helen <nwagbosohelen8@gmail.com>
2026-01-17 19:47:06 +0100

    Assignment done

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def about_us(request):
+    return render(request, 'about_us.html')
--- proj/urls.py
+++ proj/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),  
+    path('about_us/', views.about_us, name='about_us'),
 ]
